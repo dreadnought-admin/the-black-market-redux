e1842d5ef9b3bb0750116d7bf1672e05a2b49ff5
dom alexander <dom.alexan@gmail.com>
2023-04-27 21:13:25 -0600

    Adds detail page for records

--- client/src/components/RecordCard.tsx
+++ client/src/components/RecordCard.tsx
@@ -1,4 +1,4 @@
-import { Link  } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { SetStateAction, Dispatch, useState, useContext } from 'react';
 import { Record as RecordType} from '../types';
 import { RecordsContext } from '../global/RecordsProvider';
@@ -12,6 +12,8 @@ import { RecordsContext } from '../global/RecordsProvider';
 
   const RecordCard: React.FC<RecordProps> = ({ record, records, setRecords }) => {
 
+    let navigate = useNavigate();
+
     function handleRecordDelete(id: number) {
       const updatedRecords = records.filter((ogRecord) => ogRecord.id !== id);
       setRecords(updatedRecords);
@@ -21,7 +23,7 @@ import { RecordsContext } from '../global/RecordsProvider';
   return (
     <div>
         <span>
-        <Link to={`http://localhost:3000/records/${record!.id}`}>
+        <Link to={`http://localhost:5173/records/${record!.id}`}>
           {record.album_cover ? (
             <img height="75x" width="75px" src={record.album_cover}></img>
           ) : <p>Loading...</p>
--- client/src/components/RecordDetail.tsx
+++ client/src/components/RecordDetail.tsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import { useEffect, useState, useContext } from 'react';
+import { useParams, Link } from 'react-router-dom';
+import Spotify from 'react-spotify-embed';
+import { RecordsContext } from '../global/RecordsProvider';
+import { Record as RecordType } from '../types';
 
-const RecordDetail = () => {
+const RecordDetail: React.FC = () => {
+
+  const { records, setRecords } = useContext(RecordsContext);
+
+  const [recordDetail, setRecordDetail] = useState([]);
+
+  const { id } = useParams();
+
+  useEffect(() => {
+    fetch(`http://localhost:3000/records/${id}`)
+    .then((r) => r.json())
+    .then((details) => {
+      setRecordDetail(details)
+    })
+  }, [])
+
+  const { user, genre_id, album_name, 
+    artist_name, album_cover, condition, 
+    release_date, release_description, record_labels, 
+    spotify_link, price } = recordDetail
+
+
   return (
-    <div>RecordDetail</div>
+    <section>
+      <p>{album_name}</p>
+    </section>
   )
 }
 
